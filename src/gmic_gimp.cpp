6cee2a9b0618b1119efb20b63af90bf82bd6cb7e
dtschump <David.Tschumperle@gmail.com>
2016-01-29 15:10:11 +0100

    .

--- src/gmic_gimp.cpp
+++ src/gmic_gimp.cpp
@@ -516,7 +516,7 @@ const char *t(const char *const s) {
     _t("Normal","Normale");
     _t("Large","Groot");
     _t(" Available filters (%u)"," Beschikbare filters (%u)");
-    _t("Update","Actualitzaci\303\263");
+    _t("Update","Update");
     _t("Rename","Hernoemen");
     _t("Add filter to faves","Filter toevoegen aan favorieten");
     _t("Remove filter from faves","Verwijder filter uit favorieten");
